4d64e57a3013a3cf3844d11656a451ffe5ab7af3
Win94js <winthuaung6996@gmail.com>
2025-09-25 11:42:58 +0630

    fix key

--- client/dozzy/src/utils/baseUrl.js
+++ client/dozzy/src/utils/baseUrl.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = 'http://localhost:5030/api/v1';
-export const API_BASE_URL = 'https://mini-pos-project-3.onrender.com'
+export const API_BASE_URL = 'https://mini-pos-project-3.onrender.com/api/v1'
